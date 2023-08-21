556fcb2f9ae74570ff3bd9e9b5ed07649ba05303
Areli Rangel <sheccidzavala94@gmail.com>
2023-08-20 19:55:39 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// const hiddenElements = document.querySelectorAll(".hidden");
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//        if(entry.isIntersecting){
+//               entry.target.classList.add("show")
+//        } else {
+//         entry.target.classList.add("show")
+//              }      // Tu código para manejar la intersección aquí
+//     });
+// }, {
+//     threshold: 0.5,
+// });
+
+// hiddenElements.forEach((element) => observer.observe(element));
